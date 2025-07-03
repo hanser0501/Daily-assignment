03cdd2342488bfaf5c884155b3e278a648367466
cjd <3438712488@qq.com>
2025-07-03 10:04:14 +0800

    finish the second assignment

--- day2/assignment/cifar10_code_to_start_with.py
+++ day2/assignment/cifar10_code_to_start_with.py
@@ -7,6 +7,11 @@ Train a simple convolutional neural network (CNN) to classify CIFAR images.
 # %%
 # importing
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 import numpy as np
 from matplotlib import pyplot as plt
 # %%
@@ -19,6 +24,18 @@ test_images, test_labels = test_dataset.data, test_dataset.targets
 # %%
 # Code here!
 # inspect images
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+def show_sample_images(images, labels, class_names):
+    plt.figure(figsize=(8, 8))
+    for i in range(9):
+        plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i])
+        plt.title(class_names[labels[i]])
+        plt.axis('off')
+    plt.show()
+
+show_sample_images(train_images, train_labels, class_names)
 
 # %%
 # prepocess dataset (include dataloader)
@@ -27,12 +44,73 @@ test_images, test_labels = test_dataset.data, test_dataset.targets
 (sometimes dont't needed, eg: when using "sparse_categorical_crossentropy" as the loss function in model.compile, we don't need to transfrom the labels, cause this loss function expects integer labels and it does the one-hot encoding it self(sparse_))  
 2. Normalize the pixal data, scaling the pixel data from [0,255] to range [0,1]
 '''
+transform = transforms.Compose([
+    transforms.ToTensor(),  # Converts to [0,1] and rearranges dimensions to [C,H,W]
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize to [-1,1]
+])
 
+train_dataset = datasets.CIFAR10(root='./data', train=True, transform=transform)
+test_dataset = datasets.CIFAR10(root='./data', train=False, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 # %%
 # create a CNN model
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)  # output: [32, 32, 32]
+        self.pool = nn.MaxPool2d(2, 2)                           # output: [32, 16, 16]
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1) # output: [64, 16, 16]
+        self.fc1 = nn.Linear(64 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # -> [batch, 32, 16, 16]
+        x = self.pool(F.relu(self.conv2(x)))  # -> [batch, 64, 8, 8]
+        x = x.view(-1, 64 * 8 * 8)            # flatten
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+model = SimpleCNN()
 
 # %%
 # train the model
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
 
+criterion = nn.CrossEntropyLoss()  # no need for one-hot
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+epochs = 10
+for epoch in range(epochs):
+    running_loss = 0.0
+    model.train()
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss/len(train_loader):.4f}")
 # %%
 # evaluate the model
+correct = 0
+total = 0
+model.eval()
+with torch.no_grad():
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Test Accuracy: {100 * correct / total:.2f}%")
